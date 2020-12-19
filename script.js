db30f118a6aee8a263f84cd4e2e5343513699392
FokinAlexey <a.fokin1979@gmail.com>
2020-12-19 20:45:14 +0300

    restore header

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+
+
+// var swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   loop: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+// });
